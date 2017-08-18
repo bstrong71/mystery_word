0de38bd4b96045bf8b36e9b75749166f94f4381b
Bernard Strong <bstrong71@gmail.com>
2017-08-18 13:55:47 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express             = require('express');
+const path                = require('path');
+const mustacheExpress     = require('mustache-express');
+const bodyParser          = require('body-parser');
+const expressValidator    = require('express-validator');
+const morgan              = require('morgan');
+const session             = require('express-session');
+const routes              = require('./routes/index');
+const app                 = exress();
+
+app.engine("mustache", mustacheExpress);
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "mustache");
+app.set("layout", "layout");
+
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
+
+app.use(morgan("dev"));
+
+app.use(session({
+  secret: "secrets",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(routes);
+
+app.listen(3000, function() {
+  console.log("This app is running on localhost: 3000");
+});
